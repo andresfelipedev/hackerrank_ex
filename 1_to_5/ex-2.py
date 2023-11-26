ffd7f968a1bdc54bd6a8a331ec00cf886f3508f3
Felipe Moreno <f.m10102000@gmail.com>
2023-11-26 15:44:21 -0500

    first part done

--- 1_to_5/ex-2.py
+++ 1_to_5/ex-2.py
@@ -7,15 +7,24 @@ Note: If there are multiple students with the second lowest grade,
 order their names alphabetically and print each name on a new line.
 
 """
+##### array real
+real_arr = [["Harry",37.21],["Berry",37.21],["Tina",37.2],["Akriti",41],["Harsh",39]]
+
+
+#####
 
 
 
 def doing_array(arr):
-    for item in range(len(arr)):
-        print(arr[item][1])
+    order_arr = sorted(arr, key = lambda x: (x[1], x[0]))
+    print(order_arr)
+    for item in range(len(order_arr)):
+        if item <= 1:
+            print(order_arr[item][0])
 
+doing_array(real_arr)
 
-
+"""
 def main():
     students_and_grades = []
     for i in range(0,3):
@@ -31,3 +40,4 @@ def main():
 
 
 main()
+"""
